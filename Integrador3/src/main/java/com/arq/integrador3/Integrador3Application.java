3ae52982b3c7a652db911c7dcfaecb6a79466ee0
fgirolami8 <90563389+fgirolami8@users.noreply.github.com>
2022-11-22 13:59:28 -0300

    Update Integrador3Application.java

--- Integrador3/src/main/java/com/arq/integrador3/Integrador3Application.java
+++ Integrador3/src/main/java/com/arq/integrador3/Integrador3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Integrador3Application {
 
 	public static void main(String[] args) {
+		System.setProperty("maven.version", "3.6.2");
 		SpringApplication.run(Integrador3Application.class, args);
 	}
 
